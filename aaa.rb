b0b4cc53e86929ca0f01ad556f943de141d9922a
ibrahim Ethem Akbıyık <21060671@stu.omu.edu.tr>
2021-11-06 23:12:21 +0300

    deneme

--- aaa.rb
+++ aaa.rb
@@ -1,15 +1,24 @@
 def hesapoyun()
-    x = rand(10)
+    print "0 dan kaça kadar tahmin etmek istersiniz :"
+    m = gets.chomp.to_i
+    x = rand(m)
+    hak = m*3/10.round
+    puan = 100
+    sayaç = hak
+    puan_düsürücü = 100/hak
+    print "TANIMLANAN HAK : #{hak} \n "
     loop do
-        print "bir sayı tahmin ediniz 1 ile 10 arasında "
-        value = gets.chomp
+        puan = puan - puan_düsürücü
+        print "bir sayı tahmin ediniz 0 ile #{m} arasında :"
+        value = gets.chomp.to_i
+        hak -= 1
         if value == x 
-            puts "bildiniz"
+            puts "TEBRİKLER , bildiniz PUAN DURUMU : #{puan} "
             break
-        else
-            value
+        elsif  hak == 0
+            puts "maalesef hakkınız bitti , bir sonraki sefere :D"
+            break
         end        
     end
 end
-
 hesapoyun
