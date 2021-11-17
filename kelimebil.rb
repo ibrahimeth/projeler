6a687ea4d0ef60bfeabed035eeb4cb18f71305bc
ibrahim Ethem Akbıyık <21060671@stu.omu.edu.tr>
2021-11-17 14:04:06 +0300

    open to development

---
+++ kelimebil.rb
@@ -0,0 +1,28 @@
+def hesapoyun()
+  print "0 dan kaça kadar tahmin etmek istersiniz :"
+  m = gets.chomp.to_i
+  x = rand(m)
+  hak = m*3/10.round
+  sayaç = 0
+  puan_düsürücü = 100/hak
+  print "TANIMLANAN HAK : #{hak} \n "
+  loop do
+      sayaç += 1
+      puan = 100 - puan_düsürücü * (sayaç -1)
+      print "bir sayı tahmin ediniz 0 ile #{m} arasında :"
+      value = gets.chomp.to_i
+      hak -= 1
+      if value == x 
+          puts "TEBRİKLER , bildiniz PUAN DURUMU : #{puan} "
+          break
+      elsif  hak == 0
+          puts "maalesef hakkınız bitti , sayınız = #{x} idi , bir sonraki sefere :D"
+          break
+      elsif value < x
+          puts "YUKARI"
+      elsif value > x
+          puts "AŞAĞI"
+      end        
+  end
+end
+hesapoyun
